9178e29823c82b0e72e30000b27df7ecfd9d2f55
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-08 15:36:43 -0500

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Faizaan Khan on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
