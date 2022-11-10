1a95b5d752adebdfbe260863753bd61cda0a652b
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-09 20:23:00 -0500

    Time unit converter

---
+++ README.md
@@ -0,0 +1,7 @@
+# Time Unit Converter
+
+# SwiftUI app to convert time units
+
+- The user can input the value they want to convert
+- User can choose from seconds, minutes, hours, days unit
+- The output will be converted and displayed
--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var inputNumber = 0
+    @State private var inputUnit = "Minutes"
+    @State private var outputUnit = "Seconds"
+    @FocusState private var inputFocused: Bool
+        
+    let timeUnits: [String] = ["Seconds", "Minutes", "Hours", "Days"]
+    
+    var convertedOutput: Double{
+        let input = Double(inputNumber)
+        var inputInSeconds: Double{
+            switch(inputUnit){
+            case ("Seconds"):
+                return input
+            case("Minutes"):
+                return input * 60
+            case("Hours"):
+                return input * (60*60)
+            case("Days"):
+                return input * (24 * 60 * 60)
+            default:
+                return 0
+            }
         }
-        .padding()
+        
+        var convertedOutput: Double{
+            switch(outputUnit){
+            case("Seconds"):
+                return inputInSeconds
+            case("Minutes"):
+                return inputInSeconds / 60
+            case("Hours"):
+                return inputInSeconds / (60*60)
+            case("Days"):
+                return inputInSeconds / (24*60*60)
+            default:
+                return 0
+            }
+        }
+        
+        return convertedOutput
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter the number", value: $inputNumber, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputFocused)
+                    
+                    Picker("Input Unit", selection:$inputUnit){
+                        ForEach(timeUnits, id: \.self){
+                            Text("\($0)")
+                        }
+                    }
+                    .keyboardType(.decimalPad)
+                } header: {
+                    Text("The unit you want to convert")
+                }
+                
+                Section{
+                    Picker("Output unit", selection: $outputUnit){
+                        ForEach(timeUnits, id: \.self){
+                            Text("\($0)")
+                        }
+                    }
+                    
+                } header: {
+                    Text("Output")
+                }
+                
+                Section{
+                    Text("\(String(format: "%.2f", convertedOutput)) \(outputUnit)")
+                } header: {
+                    Text("Ouptut value")
+                }
+            }
+            .navigationTitle("Time Converter")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    
+                    Button("Done"){
+                        inputFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
